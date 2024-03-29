16d3a5a41ec35fde28a19548b64f8912bed2e177
Daniil <4i.danila@gmail.com>
2024-03-30 02:28:15 +0300

    Card added

---
+++ src/components/Place/components/Feature/interfaces.ts
@@ -0,0 +1,3 @@
+export interface IFeatureProps {
+    text: string;
+}
---
+++ src/components/Place/components/Info/interfaces.ts
@@ -0,0 +1,6 @@
+import {ReactElement} from "react";
+
+export interface IInfoProps {
+    icon: ReactElement;
+    text: string;
+}
