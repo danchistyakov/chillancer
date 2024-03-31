1fe11055fae743bd7055fa086a01cb0be9f9f2e8
Daniil <4i.danila@gmail.com>
2024-03-31 13:12:44 +0300

    Drawer fix Logo fix

--- src/components/Place/components/Slider/index.tsx
+++ src/components/Place/components/Slider/index.tsx
@@ -12,19 +12,19 @@ const Slider = () => {
             slidesPerView={1.5}
         >
             <SwiperSlide className={styles.slide}>
-                <img
+                <img data-vaul-no-drag={true}
                     src='https://avatars.mds.yandex.net/get-altay/10963815/2a0000018dfa5c196a1e2dbdfc985964f6d2/XXXL'/>
             </SwiperSlide>
             <SwiperSlide className={styles.slide}>
-                <img
+                <img data-vaul-no-drag={true}
                     src='https://avatars.mds.yandex.net/get-altay/11937297/2a0000018e0fe8940cc55f005fe003c8c29d/XXXL'/>
             </SwiperSlide>
             <SwiperSlide className={styles.slide}>
-                <img
+                <img data-vaul-no-drag={true}
                     src='https://avatars.mds.yandex.net/get-altay/11860411/2a0000018dfa0730189e53d05d68f4237685/XXXL'/>
             </SwiperSlide>
             <SwiperSlide className={styles.slide}>
-                <img
+                <img data-vaul-no-drag={true}
                     src='https://avatars.mds.yandex.net/get-altay/11403733/2a0000018e0b0f0f25acd014cadfaf182fb4/XXXL'/>
             </SwiperSlide>
         </Swiper>
